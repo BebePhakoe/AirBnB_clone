b43f373309c628141e4d361d3df7f842e9940ca9
Matshusa Tshumelo <tshumelomatshusa@gmail.com>
2023-12-10 05:10:58 -0800

    added the base_model init file

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines the amenity class"""
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Stads for Amenity
+
+    Attributes:
+        name (str): The namme of the amenity
+    """
+    name = ""
